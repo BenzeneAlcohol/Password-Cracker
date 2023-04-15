f26704bbfa56490d8f4a4317ccbf504c18ebfc41
ut <utkarshrm568@gmail.com>
2023-04-15 14:57:02 +0530

    Results

---
+++ Results/config_in.tcl
@@ -0,0 +1,10 @@
+set ::env(PDK) {sky130A}
+set ::env(PDKPATH) {/home/ut/.volare/sky130A}
+set ::env(STD_CELL_LIBRARY) {sky130_fd_sc_hd}
+set ::env(SCLPATH) {/home/ut/.volare/sky130A/sky130_fd_sc_hd}
+set ::env(DESIGN_DIR) {/openlane/designs/password_cracker}
+set ::env(DESIGN_NAME) {password_cracker}
+set ::env(VERILOG_FILES) {designs/password_cracker/password_cracker.v designs/password_cracker/m_digest.v designs/password_cracker/m_pader_parser.v designs/password_cracker/m_scheduler.v designs/password_cracker/top_sha.v designs/password_cracker/interative_processing.v}
+set ::env(CLOCK_PORT) {clk}
+set ::env(CLOCK_PERIOD) {10.0}
+set ::env(DESIGN_IS_CORE) {1}
